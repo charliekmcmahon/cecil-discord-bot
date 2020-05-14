ad8980f64e55a3c39217263e233384ee097dafe9
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-05-14 22:27:08 +1000

    update files

--- config.js
+++ config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  rules: {
-    '632674590849630219': {
-      channelId: '258314939998011393',
-      isUnique: true,
-      emojiRoleMap: {
-        '🎉': ['542138999511187486']
-      }
-    }
-  }
+  token: process.env.TOKEN || require('../token').TOKEN
 };
